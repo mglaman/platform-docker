85e0d151ae49dac872a63231334fbfbfb336dea2
Matt Glaman <nmd.matt@gmail.com>
2016-03-02 08:19:48 -0600

    Move browser launch logic into a trait

--- src/Command/LinkCommand.php
+++ src/Command/LinkCommand.php
@@ -9,6 +9,7 @@
 namespace mglaman\PlatformDocker\Command;
 
 use mglaman\Docker\Compose;
+use mglaman\PlatformDocker\BrowserTrait;
 use mglaman\PlatformDocker\Command\Docker\DockerCommand;
 use mglaman\PlatformDocker\Platform;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,6 +18,8 @@ use mglaman\Docker\Docker;
 
 class LinkCommand extends DockerCommand
 {
+    use BrowserTrait;
+
     /**
      * {@inheritdoc}
      */
@@ -46,46 +49,6 @@ class LinkCommand extends DockerCommand
             }
         } catch (\Exception $e) { }
 
-        $this->openUrl($url);
-    }
-
-    /**
-     * Open a URL in the browser, or print it.
-     *
-     * @param string          $url
-     */
-    protected function openUrl($url)
-    {
-        $browser = $this->getDefaultBrowser();
-        if ($browser) {
-            $opened = $this->getHelper('process')->run($this->stdOut, array($browser, $url));
-            if ($opened) {
-                $this->stdErr->writeln("<info>Opened</info>: $url");
-                return;
-            }
-        } else {
-            $this->stdErr->writeln("<error>Browser not found: $browser</error>");
-        }
-        $this->stdOut->writeln($url);
-    }
-
-    /**
-     * Find a default browser to use.
-     *
-     * @return string|false
-     */
-    protected function getDefaultBrowser()
-    {
-        $potential = array('xdg-open', 'open', 'start');
-        /** @var \Symfony\Component\Console\Helper\ProcessHelper $process */
-        $process = $this->getHelper('process');
-        foreach ($potential as $browser) {
-            // Check if command exists by executing help flag.
-
-            if ($process->run($this->stdOut, "command -v $browser")->isSuccessful()) {
-                return $browser;
-            }
-        }
-        return false;
+        $this->openUrl($url, $this->stdErr, $this->stdOut);
     }
 }
